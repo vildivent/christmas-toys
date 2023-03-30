27724e7408bf1946987ad305a8f19a656447f20f
vildivent <vildivent@mail.ru>
2023-03-30 16:45:02 +0300

    fix

--- src/widgets/Navbar/index.tsx
+++ src/widgets/Navbar/index.tsx
@@ -1,22 +1,26 @@
+import { useToysQuery } from "entities/Toy/lib/store";
 import User from "../../entities/User/components/User";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { AiOutlineExclamation } from "react-icons/ai";
 import { LogoutBtn, SearchBtn, MenuBtn, LoginBtn } from "shared/ui/buttons";
 import { useFilterCardStore } from "widgets/ToyFilter/lib/store";
 
 const Navbar = () => {
   const { data: sessionData } = useSession();
   const { setIsOpen } = useFilterCardStore();
+  const { query } = useToysQuery();
 
   return (
-    <nav
-      className={`flex w-full items-center bg-gray-2/80 px-5 py-2 ${
-        sessionData?.user.role === "Admin" ? "justify-between" : "justify-end"
-      }`}
-    >
+    <nav className="flex w-full items-center justify-between bg-gray-2/80 px-5 py-2">
       <div className="flex gap-5">
         <MenuBtn onClick={() => null} />
         {sessionData?.user.role === "Admin" && (
-          <SearchBtn onClick={() => setIsOpen((state) => !state)} />
+          <div className="flex">
+            <SearchBtn onClick={() => setIsOpen((state) => !state)} />
+            {query && (
+              <AiOutlineExclamation className="translate-x-[-10px] translate-y-[-5px] text-lg text-red-500" />
+            )}
+          </div>
         )}
       </div>
 
